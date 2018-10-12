e07898ecd9904fe0888e71af273eb65ca06a17d8
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-13 01:34:30 +0300

    Conditional Validation

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/user-name.validator'
 import { PasswordValidator } from './shared/password.validator';
 
@@ -8,24 +8,46 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  registrationForm: FormGroup;
 
   get userName() {
     return this.registrationForm.get('userName')
   }
 
+  get email() {
+    return this.registrationForm.get('email')
+  }
+
   constructor(private fb: FormBuilder) {}
-  
-  registrationForm = this.fb.group({
-    userName: ['Rani', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/admin/), forbiddenNameValidator(/password/)]],
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-  }, {validator: PasswordValidator})
+
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: ['Rani', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/admin/), forbiddenNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    }, {validator: PasswordValidator})
+
+    this.registrationForm.get('subscribe').valueChanges.subscribe(
+      checkedValue => {
+        const email = this.registrationForm.get('email');
+        if (checkedValue) {
+          email.setValidators(Validators.required);
+        } else {
+          email.clearValidators();
+        }
+        email.updateValueAndValidity();
+      }
+    )
+  }
 
   loadAPIData() {
     this.registrationForm.setValue({
